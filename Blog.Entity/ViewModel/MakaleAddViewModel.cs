3c3ac5c7afb036ad8635a7e297aed42396691655
M720694@outlook.com <M720694@outlook.com>
2019-03-06 19:56:55 +0300

    makale ekleme işlevi katıldı

--- Blog.Entity/ViewModel/MakaleAddViewModel.cs
+++ Blog.Entity/ViewModel/MakaleAddViewModel.cs
@@ -1,12 +1,28 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Web;
 
 namespace Blog.Entity.ViewModel
 {
-    class MakaleAddViewModel
+    public class MakaleAddViewModel
     {
+        [Required(ErrorMessage = "Lütfen makale başlığı giriniz!")]
+        [StringLength(100, ErrorMessage = "Başlık {0} karakterden uzun olmamalıdır!")]
+        public string Title { get; set; }
+        [Required]
+        [DataType(DataType.Html)]
+        public string Summary { get; set; }
+        [Required]
+        [DataType(DataType.Html)]
+        public string Content { get; set; } //ckeditor den gelecek
+        [StringLength(100)]
+        public string Picture { get; set; } //görüntünün adı için
+        public int CategoryId { get; set; }
+        public string UserId { get; set; }
+        public HttpPostedFileBase PictureUpload { get; set; } //controller a göndermemiz için gereken bir property. tek görüntü için bu, çoklu için List<HttpPostedFileBase> . eğer ki view içinde type ı file olan bir input kullanıyorsak, onun ismi,uzantsı,yolu bilgilerini çekebilmek için bu türden bir property kullanıyoruz. bir çok property geleceği için, bu şekilde bir property yaptık. sadece görüntü yüklemeye yarayan bir form olsaydı, model yazmazdık; post controller ında parametre olarak HttpPostedFileBase gelirdi.
     }
 }
--- Blog.PL/Controllers/MakaleController.cs
+++ Blog.PL/Controllers/MakaleController.cs
@@ -1,21 +1,64 @@
 ﻿using Blog.BLL.Repository;
 using Blog.DAL.Context;
 using Blog.Entity.Entity;
+using Blog.Entity.ViewModel;
+using Microsoft.AspNet.Identity;
 using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
 
 namespace Blog.PL.Controllers
 {
-    public class MakaleController : Controller
+    public class MakaleController : BaseController
     {
         Repository<Article> repoM = new Repository<Article>(new BlogContext());
         public ActionResult Index()
         {
+            return View(repoM.GetAll());
+        }
+
+        public ActionResult Ekle()
+        {
             return View();
         }
+
+        [HttpPost,ValidateInput(false)] //validate input false demezsek ckdan gelen htmlleri geçirmiyor.
+        public ActionResult Ekle(MakaleAddViewModel model)
+        {
+            if (model.PictureUpload != null)
+            {
+
+                /*
+                 * string name = Path.GetFileNameWithoutExtension(model.PictureUpload.FileName);
+                name = name.Replace(" ", ""); mesela içerdeki boşlukları yoket.
+                string ext = Path.GetExtension(model.PictureUpload.FileName);
+                bu ikiliyle alınanı işlemeye yarıyor System.IO kullanarak
+                */
+
+                //resim upload edilecek.
+                string filename = model.PictureUpload.FileName; //gelen dosya adı ne ise o şekilde kayıt
+                string imagepath = Server.MapPath("/images/" + filename);
+                model.PictureUpload.SaveAs(imagepath);
+                //makale eklenecek
+                Article yeni = new Article();
+                yeni.Title = model.Title;
+                yeni.Summary = model.Summary;
+                yeni.Content = model.Content;
+                yeni.Picture = filename;
+                yeni.CategoryId = model.CategoryId;
+                //yeni.UserId = HttpContext.User.Identity.GetUserId() şu an aktif yok;
+                yeni.UserId = "55b5fccd-fbd5-4f3a-9fc0-b4b0714cc96d";
+                if (repoM.Add(yeni))
+                    return RedirectToAction("Index");
+                else
+                    return View(model);
+            }
+            return View();
+        }
+
         public ActionResult MakaleDetay(int Id)
         {
             //Id'ye göre makaleyi çağır, view'a gönder...
