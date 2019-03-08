8e77cad3ca6f732abc970c5d90c39134afdaeece
M720694@outlook.com <M720694@outlook.com>
2019-03-08 16:12:31 +0300

    07.03 hocanın değişiklikleri alındı

--- Blog.PL/Controllers/MakaleController.cs
+++ Blog.PL/Controllers/MakaleController.cs
@@ -19,50 +19,42 @@ namespace Blog.PL.Controllers
         {
             return View(repoM.GetAll());
         }
-
         public ActionResult Ekle()
         {
             return View();
         }
-
-        [HttpPost,ValidateInput(false)] //validate input false demezsek ckdan gelen htmlleri geçirmiyor.
+        [HttpPost, ValidateInput(false)]
         public ActionResult Ekle(MakaleAddViewModel model)
         {
-            if (model.PictureUpload != null)
+            if(model.PictureUpload != null)
             {
-
-                /*
-                 * string name = Path.GetFileNameWithoutExtension(model.PictureUpload.FileName);
-                name = name.Replace(" ", ""); mesela içerdeki boşlukları yoket.
-                string ext = Path.GetExtension(model.PictureUpload.FileName);
-                bu ikiliyle alınanı işlemeye yarıyor System.IO kullanarak
-                */
-
-                //resim upload edilecek.
-                string filename = model.PictureUpload.FileName; //gelen dosya adı ne ise o şekilde kayıt
-                string imagepath = Server.MapPath("/images/" + filename);
-                model.PictureUpload.SaveAs(imagepath);
-                //makale eklenecek
+                //string name = Path.GetFileNameWithoutExtension(model.PictureUpload.FileName);
+                //string ext = Path.GetExtension(model.PictureUpload.FileName);
+                ////Resim upload edilecek.
+                //name = name.Replace(" ", "");
+                string filename = model.PictureUpload.FileName;
+                string imagePath = Server.MapPath("/images/" + filename);
+                model.PictureUpload.SaveAs(imagePath);
+                //Makale eklenecek.
                 Article yeni = new Article();
                 yeni.Title = model.Title;
                 yeni.Summary = model.Summary;
                 yeni.Content = model.Content;
                 yeni.Picture = filename;
                 yeni.CategoryId = model.CategoryId;
-                //yeni.UserId = HttpContext.User.Identity.GetUserId() şu an aktif yok;
+                //yeni.UserId = HttpContext.User.Identity.GetUserId();
                 yeni.UserId = "55b5fccd-fbd5-4f3a-9fc0-b4b0714cc96d";
                 if (repoM.Add(yeni))
                     return RedirectToAction("Index");
-                else
-                    return View(model);
+                return View(model);
             }
+
+
             return View();
         }
-
         public ActionResult MakaleDetay(int Id)
-        {
-            //Id'ye göre makaleyi çağır, view'a gönder...
-            return View();
+        {            
+            return View(repoM.GetById(Id));
         }
     }
 }
