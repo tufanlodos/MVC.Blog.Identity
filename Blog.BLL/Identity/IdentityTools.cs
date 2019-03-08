78a3def0e2d5be7c0af640b05b17f4bd4687d664
M720694@outlook.com <M720694@outlook.com>
2019-03-08 18:22:52 +0300

    çok kullanımlık identity işlemleri için class oluşturuldu.

---
+++ Blog.BLL/Identity/IdentityTools.cs
@@ -0,0 +1,20 @@
+﻿using Blog.DAL.Context;
+using Blog.Entity.Identity;
+using Microsoft.AspNet.Identity;
+using Microsoft.AspNet.Identity.EntityFramework;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Blog.BLL.Identity
+{
+    public static class IdentityTools //bir çok yerde user role işlemleri için bunlar lazım olduğunda çekebilecez.
+    {
+        public static UserStore<ApplicationUser> NewUserStore() => new UserStore<ApplicationUser>(new BlogContext());
+        public static UserManager<ApplicationUser> NewUserManager() => new UserManager<ApplicationUser>(NewUserStore());
+        public static RoleStore<ApplicationRole> NewRoleStore() => new RoleStore<ApplicationRole>(new BlogContext());
+        public static RoleManager<ApplicationRole> NewRoleManager() => new RoleManager<ApplicationRole>(NewRoleStore());
+    }
+}
